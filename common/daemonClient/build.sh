160fe3f549fed53ce2c2fa2c653985d57868e927
RowlingWu <438375422@qq.com>
2018-12-21 15:08:47 +0800

    delete no-use files

--- common/daemonClient/build.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p gen-cpp
-rm daemon_client gen-cpp/* -rf
-
-protoc -I ../../daemonServer/grpc --grpc_out=gen-cpp --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` ../../daemonServer/grpc/daemonServer.proto
-protoc -I ../../daemonServer/grpc --cpp_out=gen-cpp ../../daemonServer/grpc/daemonServer.proto
-
-make
