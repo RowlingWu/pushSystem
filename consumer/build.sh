1457821c50fc97e49d12675115b4e4a8f443ef3f
RowlingWu <438375422@qq.com>
2019-03-04 18:21:41 +0800

    modified .gitignore

--- consumer/build.sh
+++ consumer/build.sh
@@ -1,5 +1,5 @@
 mkdir -p gen-cpp
-rm consumer gen-cpp/* -rf
+rm consumerd gen-cpp/* -rf
 
 protoc -I grpc --grpc_out=gen-cpp --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` grpc/consumer.proto
 protoc -I grpc --cpp_out=gen-cpp grpc/consumer.proto
--- producer/build.sh
+++ producer/build.sh
@@ -1,5 +1,5 @@
 mkdir -p gen-cpp
-rm producer gen-cpp/* -rf
+rm producerd gen-cpp/* -rf
 
 protoc -I grpc --grpc_out=gen-cpp --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` grpc/producer.proto
 protoc -I grpc --cpp_out=gen-cpp grpc/producer.proto
